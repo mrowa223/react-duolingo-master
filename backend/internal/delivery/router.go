4a5d481293ba263d52ccdf6af752715600bb3409
eegurt <40899803+eegurt@users.noreply.github.com>
2024-10-19 22:56:01 +0500

    fix: cors

--- backend/internal/delivery/router.go
+++ backend/internal/delivery/router.go
@@ -14,13 +14,5 @@ func NewHttpRouter(httpHandler httpHandler) http.Handler {
 	router.Handle(http.MethodPost, "/v1/llm/help-text", httpHandler.GenerateHelpTextHandler)
 	// router.Handle(http.MethodPost, "/v1/tgbot/send-token", httpHandler.SendTgbotToken)
 
-	// Enable CORS
-	corsHandler := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:5001"},                   // Allow frontend origin
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}, // Allow specific methods
-		AllowedHeaders:   []string{"Content-Type", "Authorization"},
-		AllowCredentials: true, // If you need credentials (cookies, etc.)
-	})
-
-	return recoverPanic(logMiddleware(corsHandler.Handler(router)))
+	return recoverPanic(logMiddleware(cors.AllowAll().Handler(router)))
 }
